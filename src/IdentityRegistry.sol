fe0be56fa325fcc3a33ff26ec43fe1866a8e7ae5
Uk2476 <kuttkarsh028@gmail.com>
2026-04-12 18:52:28 +0530

    IdentityRegistry : added a mapping to store all the revoked address and also added few condition checks

--- src/IdentityRegistry.sol
+++ src/IdentityRegistry.sol
@@ -8,6 +8,7 @@ contract IdentityRegistry is IIdentityRegistry {
     address public owner;
     mapping(address => bytes32) private _identityHash;
     mapping(address => bool) private _active;
+    mapping(address => bool) private _revoked;
 
     modifier onlyOwner() {
         if (msg.sender != owner) revert Unauthorized();
@@ -15,16 +16,23 @@ contract IdentityRegistry is IIdentityRegistry {
     }
 
     constructor(address _owner) {
+        if (_owner == address(0)) revert InvalidAddress();
         owner = _owner;
     }
 
     function registerIdentity(bytes32 identityHash) external {
+        if (identityHash == bytes32(0)) revert InvalidBytes32();
+        if (_revoked[msg.sender]) revert RevokedAddress();
+        if (_active[msg.sender]) revert AlreadyExists();
         _identityHash[msg.sender] = identityHash;
         _active[msg.sender] = true;
         emit IdentityRegistered(msg.sender, identityHash, msg.sender, block.timestamp);
     }
 
     function registerIdentityFor(address user, bytes32 identityHash) external onlyOwner {
+        if (identityHash == bytes32(0)) revert InvalidBytes32();
+        if (_revoked[user]) revert RevokedAddress();
+        if (_active[user]) revert AlreadyExists();
         _identityHash[user] = identityHash;
         _active[user] = true;
         emit IdentityRegistered(user, identityHash, msg.sender, block.timestamp);
@@ -34,6 +42,7 @@ contract IdentityRegistry is IIdentityRegistry {
         if (msg.sender != owner && msg.sender != user) revert Unauthorized();
         delete _identityHash[user];
         _active[user] = false;
+        _revoked[user] = true;
         emit IdentityRevoked(user, msg.sender, block.timestamp);
     }
 
